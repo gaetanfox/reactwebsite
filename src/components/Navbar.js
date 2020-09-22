5f3eb1e1c04b1895b21c10cd3eaf670d1ff3b17e
Gaetan Fox <bgloup@gmail.com>
2020-09-22 09:25:09 +0200

    Home and cards

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+
+import './Navbar.css'
+import { Button } from './Button'
 
 function Navbar() {
   const [click, setClick] = useState(false)
+  const [button, setButton] = useState(true)
 
   const handleClick = () => {
     setClick(!click)
@@ -10,11 +14,25 @@ function Navbar() {
 
   const closeMobileMenu = () => setClick(false)
 
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  }
+
+  useEffect(() => {
+    setButton()
+  }, [])
+
+  window.addEventListener('resize', showButton)
+
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link to='/' className='navbar-logo'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             TRVL <i className='fab fa-typo3'></i>
           </Link>
           <div className='menu-icon' onClick={handleClick}>
@@ -54,6 +72,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
         </div>
       </nav>
     </>
